38dcff9a0e82a8d3da1270a041a42f260829e7ad
AJMAL <piacademyweb1@gmail.com>
2025-06-20 14:57:14 +0530

    Adding buy now button

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -4,6 +4,7 @@ import Categories from '../Categories'
 import HeroSection from '../HeroSection'
 import MegaMenu from '../MegaMenu'
 import ReadyToShip from '../ReadyToShip'
+import { Button } from '../ui/button'
 
 interface HomePageProps {
   carousal: Carousal[]
@@ -17,6 +18,11 @@ export default function HomePage({ carousal, categories }: HomePageProps) {
       <HeroSection carousal={carousal} />
       <ReadyToShip />
       <Categories categories={categories} />
+      <div className="flex justify-center w-full">
+        <Button className="relative bg-gradient-to-r from-cyan-900 to-[#36559b] px-10 rounded-[50px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] justify-start text-white text-2xl font-semibold">
+          BUY NOW
+        </Button>
+      </div>
     </div>
   )
 }
