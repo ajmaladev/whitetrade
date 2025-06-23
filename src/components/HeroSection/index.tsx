423c4e48c19d51d888db6e98617ef635e8190216
AJMAL <piacademyweb1@gmail.com>
2025-06-24 00:22:44 +0530

    responisveness is completed

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -21,7 +21,7 @@ export default function HeroSection({ carousal = [] }: HeroSectionProps) {
   return (
     <div className="relative">
       {/* Large screen design */}
-      <div className="hidden md:block">
+      <div className="hidden sm:block">
         <div className="relative w-full h-[300px] sm:h-[400px] md:h-[450px] lg:h-[500px] overflow-hidden">
           {/* Carousel background */}
           {carousal?.map((img, idx) => {
@@ -50,12 +50,12 @@ export default function HeroSection({ carousal = [] }: HeroSectionProps) {
             )
           })}
         </div>
-        <div className="absolute inset-0 top-[100px] sm:top-[120px] md:top-[150px] lg:top-[177px] flex items-center justify-center z-[2] pointer-events-none px-4 sm:px-6 md:px-8">
+        <div className="absolute inset-0 sm:top-[216px] md:top-[225px] lg:top-[177px] flex items-center justify-center z-[2] pointer-events-none px-4 sm:px-6 md:px-8">
           <div
-            className="-z-10 md:z-10 -m-32 relative w-full max-w-[320px] sm:max-w-[500px] md:max-w-[700px] lg:max-w-[963px] h-[280px] sm:h-[320px] md:h-[380px] lg:h-[452px] flex flex-col items-center justify-between pointer-events-auto overflow-hidden gap-4 sm:gap-6 md:gap-8 lg:gap-10 "
+            className="-z-10 md:z-10 -m-32 relative w-full max-w-[320px] sm:max-w-[500px] md:max-w-[700px] lg:max-w-[963px] h-[280px] sm:h-[234.5px] md:h-[329px] lg:h-[452px] flex flex-col items-center justify-between pointer-events-auto overflow-hidden gap-4 sm:gap-6 md:gap-8 lg:gap-10 "
             style={{
               backgroundImage: 'url(/hero-section-bg.webp)',
-              backgroundSize: 'cover',
+              backgroundSize: 'contain',
               backgroundPosition: 'center',
               position: 'relative',
             }}
@@ -84,7 +84,7 @@ export default function HeroSection({ carousal = [] }: HeroSectionProps) {
               <div className="pl-2 sm:pl-4 md:pl-6 justify-start text-neutral-500 text-base sm:text-lg md:text-xl lg:text-3xl font-normal">
                 over a decade now
               </div>
-              <div className="w-full mt-10 max-w-[280px] sm:max-w-[320px] md:max-w-[380px] lg:max-w-[426px] h-10 sm:h-12 md:h-13 lg:h-14 relative bg-gradient-to-r from-[#1C3A6A] to-[#3661c1] rounded-[25px] sm:rounded-[30px] md:rounded-[40px] lg:rounded-[50px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+              <div className="w-full md:mt-10 max-w-[280px] sm:max-w-[320px] md:max-w-[380px] lg:max-w-[426px] h-10 sm:h-12 md:h-13 lg:h-14 relative bg-gradient-to-r from-[#1C3A6A] to-[#3661c1] rounded-[25px] sm:rounded-[30px] md:rounded-[40px] lg:rounded-[50px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
                 <div className="absolute inset-0 flex items-center justify-center text-white text-sm sm:text-base md:text-lg lg:text-2xl font-semibold tracking-wide px-4 text-center">
                   Schedule a Call with Our Team
                 </div>
@@ -95,7 +95,7 @@ export default function HeroSection({ carousal = [] }: HeroSectionProps) {
       </div>
 
       {/* Small screen design */}
-      <div className="md:hidden m-4 relative">
+      <div className="sm:hidden m-4 relative">
         <div className="relative w-full h-[344px] overflow-hidden rounded-2xl">
           {/* Carousel with small_image */}
           {carousal?.map((img, idx) => {
@@ -117,7 +117,7 @@ export default function HeroSection({ carousal = [] }: HeroSectionProps) {
                 width={360}
                 height={170}
                 unoptimized
-                className={`absolute top-0 left-0 w-auto h-auto object-cover  transition-opacity duration-1000 z-[2] ${idx === current ? 'opacity-100' : 'opacity-0'}`}
+                className={`absolute top-0 left-0 w-auto h-auto object-cover transition-opacity duration-1000 z-[2] ${idx === current ? 'opacity-100' : 'opacity-0'}`}
               />
             )
           })}
@@ -126,14 +126,15 @@ export default function HeroSection({ carousal = [] }: HeroSectionProps) {
         </div>
       </div>
       <div
-        className="md:hidden absolute top-[43px] left-0 w-full h-[227px] flex flex-col items-center justify-center pointer-events-auto overflow-hidden rounded-b-2xl z-[1]"
+        className="hero-section-small-container sm:hidden absolute top-[43px] left-0 w-full h-[250px] flex flex-col items-center justify-center pointer-events-auto overflow-hidden rounded-b-2xl z-[1]"
         style={{
           backgroundImage: 'url(/hero-section-small-bg.webp)',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
         }}
       >
-        <div className="relative z-10 flex flex-col items-start justify-end w-full h-full gap-2 p-6">
+        <div className="hero-section-small relative mb-4 sm:mb-6 md:mb-8 z-10 flex flex-col items-start justify-end w-full h-full gap-2 p-6">
           <h1 className="text-[#1C3A6A] text-2xl font-medium font-['Poppins'] leading-tight">
             White Trading Company
           </h1>
